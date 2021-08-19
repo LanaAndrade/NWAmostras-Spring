493d1f4e2603aa7c0327e21dbf56d55f349b39dd
LanaAndrade <82892986+LanaAndrade@users.noreply.github.com>
2021-08-19 00:25:10 -0300

    Pasta inicial criada

---
+++ NWAmostras/NWAmostras/src/main/java/br/com/etechoracio/NWAmostras/NwAmostrasApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.NWAmostras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwAmostrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwAmostrasApplication.class, args);
+	}
+
+}
---
+++ NWAmostras/NWAmostras/src/test/java/br/com/etechoracio/NWAmostras/NwAmostrasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.NWAmostras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NwAmostrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
